fd545e95b8c065fd65f9d4b0b12247d4cfa929b8
Azaan Hassan <ahmedaeon@gmail.com>
2020-07-17 02:28:30 +0530

    add a base scaffolding for the app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
+import Person from "./pages/Person";
+import HomePage from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Router>
+        <Switch>
+
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+
+          <Route path="/person/:id">
+            <Person />
+          </Route>
+
+          <Route>
+            <NotFound />
+          </Route>
+
+        </Switch>
+      </Router>
     </div>
   );
 }
